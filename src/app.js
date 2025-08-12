c19fdb3881766c2ebb8c24fdf1560a6fcbc06814
Mayur Badgujar <mayurbadgujar873@gmail.com>
2025-08-12 22:32:33 +0530

    AsyncHandler added, Healhcheck api endpoint working

--- src/app.js
+++ src/app.js
@@ -1,10 +1,24 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 
 //router imports
 import healthCheckRouter from "./routes/healthcheck.routes.js";
 
-app.use("/api/v1.healthcheck", healthCheckRouter);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(
+    cors({
+        origin: process.env.BASE_URL,
+        credentials: true,
+        methods: ["GET", "POST", "PATCH", "DELETE"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+    }),
+);
+
+app.use("/api/v1/healthcheck", healthCheckRouter);
 
 export default app;
