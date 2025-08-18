6361f82535f81463a5ec835d373de4b388871873
Mayur Badgujar <mayurbadgujar873@gmail.com>
2025-08-19 00:18:34 +0530

    Add project & member API routes with RBAC middleware
    - Implemented project routes (create, fetch, update, delete)
    - Implemented member routes (add, update role, remove, list members)
    - Integrated isLoggedIn and alidateProjectPermission middleware
    - Applied role-based access control:
      • All members → view project & members
      • Admins → update/delete projects, manage members
      • Any user → create new projects

--- src/routes/project.routes.js
+++ src/routes/project.routes.js
@@ -1,5 +1,67 @@
 import { Router } from "express";
+import {
+  isLoggedIn,
+  validateProjectPermission,
+} from "../middlewares/auth.middleware.js";
+import { AvailableUserRoles, UserRolesEnum } from "../utils/constants.js";
+import {
+  addMemberToProject,
+  createProject,
+  deleteMember,
+  deleteProject,
+  getProjectById,
+  getProjectMembers,
+  getProjects,
+  updateMemberRole,
+  updateProject,
+} from "../controllers/project.controllers.js";
 
 const router = Router();
+
+router.route("/").get(isLoggedIn, getProjects).post(isLoggedIn, createProject);
+
+router
+  .route("/:projectId")
+  .get(
+    isLoggedIn,
+    validateProjectPermission(AvailableUserRoles),
+    getProjectById,
+  )
+  .put(
+    isLoggedIn,
+    validateProjectPermission([UserRolesEnum.ADMIN]),
+    updateProject,
+  )
+  .delete(
+    isLoggedIn,
+    validateProjectPermission([UserRolesEnum.ADMIN]),
+    deleteProject,
+  );
+
+router
+  .route("/:projectId/members")
+  .get(
+    isLoggedIn,
+    validateProjectPermission(AvailableUserRoles),
+    getProjectMembers,
+  )
+  .post(
+    isLoggedIn,
+    validateProjectPermission([UserRolesEnum.ADMIN]),
+    addMemberToProject,
+  );
+
+router
+  .route("/:projectId/members/:memberId")
+  .put(
+    isLoggedIn,
+    validateProjectPermission([UserRolesEnum.ADMIN]),
+    updateMemberRole,
+  )
+  .delete(
+    isLoggedIn,
+    validateProjectPermission([UserRolesEnum.ADMIN]),
+    deleteMember,
+  );
 
 export default router;
