514665c8eb8ba0886345f9cd2478e6da1a4b441e
Mayur Badgujar <mayurbadgujar873@gmail.com>
2025-12-09 00:37:56 +0530

    Now supporting the PUT method

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ app.use(
     cors({
         origin: process.env.BASE_URL,
         credentials: true,
-        methods: ["GET", "POST", "PATCH", "DELETE"],
+        methods: ["GET", "POST", "PATCH", "DELETE", "PUT", "OPTIONS"],
         allowedHeaders: ["Content-Type", "Authorization"],
     }),
 );
