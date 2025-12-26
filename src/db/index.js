9cfa4ceb73a5ce0c0ef9881ab5be79b04c57863f
Mayur Badgujar <mayurbadgujar873@gmail.com>
2025-12-26 14:20:06 +0530

    fix: configure for Vercel serverless deployment

--- src/db/index.js
+++ src/db/index.js
@@ -2,11 +2,14 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
+    if (mongoose.connection.readyState >= 1) {
+      return;
+    }
     await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB connected");
   } catch (error) {
     console.error("Mongodb connection failed", error);
-    process.exit(1);
+    throw error;
   }
 };
 
